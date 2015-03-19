ce9c9ca7905578f7b19306c52fbc69306fad342e
Michael Tomcal <mtomcal@cbtnuggets.com>
2015-03-18 19:19:14 -0700

    Added shell script

---
+++ startup.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+root_dir=`pwd`
+curl https://raw.githubusercontent.com/creationix/nvm/v0.24.0/install.sh | bash
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+ln -s .vimrc ~/
+ln -s .vim ~/
+ln -s .gvimrc ~/
+ln -s .zshrc ~/
+mkdir ~/bin
+mkdir ~/Projects
+cd ~/.vim
+ruby update_bundles
+cd $root_dir
+nvm install v0.12.0
+npm install -g gulp jshint jsxhint
+open base16-tomorrow.dark.itermcolors
+open *.ttf
