37ed365b55e4ba7e59dede81c6a50dc4d3f9cbb5
Streaming <streaming@Mtomcals-MBP.winston>
2015-03-18 19:47:21 -0700

    Fixed startup

--- startup.sh
+++ startup.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 root_dir=`pwd`
 curl https://raw.githubusercontent.com/creationix/nvm/v0.24.0/install.sh | bash
-curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
 ln -s .vimrc ~/
 ln -s .vim ~/
 ln -s .gvimrc ~/
 ln -s .zshrc ~/
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
 source ~/.zshrc
 mkdir ~/bin
 mkdir ~/Projects
 cd ~/.vim
+mkdir bundle
 ruby update_bundles
 cd $root_dir
 nvm install v0.12.0
