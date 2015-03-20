69a3848542ec45c57d30eef4d5058d2ce2a7f9ca
Streaming <streaming@Mtomcals-MacBook-Pro.local>
2015-03-19 20:33:00 -0700

    Fixes

--- startup.sh
+++ startup.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 root_dir=`pwd`
 curl https://raw.githubusercontent.com/creationix/nvm/v0.24.0/install.sh | bash
-ln -s .vimrc ~/
-ln -s .vim ~/
-ln -s .gvimrc ~/
-ln -s .zshrc ~/
+ln -s $root_dir/.vimrc ~/
+ln -s $root_dir/.vim ~/
+ln -s $root_dir/.gvimrc ~/
+ln -s $root_dir/.zshrc ~/
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
 source ~/.zshrc
 mkdir ~/bin
 mkdir ~/Projects
-cd ~/.vim
+cd .vim
 mkdir bundle
 ruby update_bundles
 cd $root_dir
