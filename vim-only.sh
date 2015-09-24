c681b2014f116d3b154885e230e63ab3f356434f
Michael Tomcal <mtomcal@cbtnuggets.com>
2015-09-24 15:43:48 -0700

    Added zsh to vim-only

--- vim-only.sh
+++ vim-only.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 root_dir=`pwd`
+rm ~/.vimrc
+rm -r ~/.vim
 ln -s $root_dir/.vimrc ~/
 ln -s $root_dir/.vim ~/
 ln -s $root_dir/.gvimrc ~/
+ln -s $root_dir/.zshrc ~/
+ln -s $root_dir/.atom ~/
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+source ~/.zshrc
 cd .vim
 mkdir bundle
 ruby update_bundles
